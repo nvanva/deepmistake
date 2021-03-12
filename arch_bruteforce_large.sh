1d2d1515c0db44bd65188385d2bc028c9d0d078f
Maksim <50339715+ameta13@users.noreply.github.com>
2021-03-12 23:07:55 +0300

    Add scripts to brute force architectures

---
+++ arch_bruteforce_large.sh
@@ -0,0 +1,21 @@
+train_epochs=$1 #50
+ft_epochs=$2 #50
+DATA_TRAIN_DIR=data_dumped_full/wic/
+DATA_FT_DIR=data_dumped_full/rusemshift-data/mean/
+grad_acc_steps=4
+train_loss=crossentropy_loss
+ft_loss=mse_loss
+pool=mean
+batch_norm=1
+
+for targ_emb in dist_l1ndotn dist_l1; do
+for hs in 0 300; do
+bash train_n_finetune_model.sh $DATA_TRAIN_DIR $DATA_FT_DIR $train_loss $ft_loss $pool $targ_emb $batch_norm $hs $train_epochs $ft_epochs $grad_acc_steps
+done
+done
+
+for targ_emb in concat comb_dmn; do
+for hs in -1; do
+bash train_n_finetune_model.sh $DATA_TRAIN_DIR $DATA_FT_DIR $train_loss $ft_loss $pool $targ_emb $batch_norm $hs $train_epochs $ft_epochs $grad_acc_steps
+done
+done
---
+++ train_n_finetune_model.sh
@@ -0,0 +1,24 @@
+DATA_TRAIN_DIR=$1 #data/wic/
+DATA_FT_DIR=$2 #data/rusemshift-data/mean/
+train_loss=$3 #crossentropy_loss
+ft_loss=$4 #mse_loss
+pool=$5 #mean
+targ_emb=$6 #concat
+batch_norm=$7 #1
+hs=$8 #300
+train_epochs=$9 #50
+ft_epochs=${10} #50
+grad_acc_steps=${11} #4
+
+OUTPUT_DIR=xlmrlarge-train_loss-${train_loss}-ft_loss-${ft_loss}-pool-${pool}-targ_emb-${targ_emb}-hs-${hs}-bn-${batch_norm}
+linhead=$([ "$hs" == 0 ] && echo "true" || echo "false")
+echo output_dir = $OUTPUT_DIR
+echo data_train = $DATA_TRAIN_DIR, data_ft = $DATA_FT_DIR
+echo train_epochs = $train_epochs, ft_epochs = $ft_epochs, grad_acc_steps = $grad_acc_steps
+
+python run_model.py --do_train --do_validation --data_dir $DATA_TRAIN_DIR --output_dir ${OUTPUT_DIR}/train/ --gradient_accumulation_steps $grad_acc_steps \
+	--pool_type $pool --target_embeddings $targ_emb --head_batchnorm $batch_norm --loss $train_loss --linear_head ${linhead} --head_hidden_size $hs \
+	--num_train_epochs $train_epochs
+python run_model.py --train_scd --do_train --do_validation --data_dir $DATA_FT_DIR --output_dir ${OUTPUT_DIR}/finetune/ --gradient_accumulation_steps $grad_acc_steps \
+	--pool_type $pool --target_embeddings $targ_emb --head_batchnorm $batch_norm --loss $ft_loss --linear_head ${linhead} --head_hidden_size $hs \
+	--num_train_epochs $ft_epochs --ckpt_path ${OUTPUT_DIR}/train/nen-nen-weights/
