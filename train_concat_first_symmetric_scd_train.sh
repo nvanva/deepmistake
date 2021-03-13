56d6e435ff20874035b7e46f444e105c1d72e651
ameta13 <maxim.fedoseev13@yandex.ru>
2021-03-13 23:40:16 +0300

    Add script to train submitted model

---
+++ train_concat_first_symmetric_scd_train.sh
@@ -0,0 +1,13 @@
+train_epochs=50
+ft_epochs=30
+DATA_TRAIN_DIR=data_dumped_full/wic/
+DATA_FT_DIR=data_dumped_full/rusemshift-data/mean/
+grad_acc_steps=4
+train_loss=crossentropy_loss
+ft_loss=mse_loss
+pool=first
+batch_norm=0
+targ_emb=concat
+hs=-1
+train_ckpt=accuracy.dev.en-en.score
+bash train_n_finetune_model.sh $DATA_TRAIN_DIR $DATA_FT_DIR $train_loss $ft_loss $pool $targ_emb $batch_norm $hs $train_epochs $ft_epochs $grad_acc_steps $train_ckpt
--- train_n_finetune_model.sh
+++ train_n_finetune_model.sh
@@ -9,8 +9,9 @@ hs=$8 #300
 train_epochs=$9 #50
 ft_epochs=${10} #50
 grad_acc_steps=${11} #4
+train_ckpt=${12} #nen-nen-weights or accuracy.dev.en-en.score
 
-OUTPUT_DIR=xlmrlarge-train_loss-${train_loss}-ft_loss-${ft_loss}-pool-${pool}-targ_emb-${targ_emb}-hs-${hs}-bn-${batch_norm}
+OUTPUT_DIR=xlmrlarge-train_loss-${train_loss}-ft_loss-${ft_loss}-pool-${pool}-targ_emb-${targ_emb}-hs-${hs}-bn-${batch_norm}-ckpt-${train_ckpt}
 linhead=$([ "$hs" == 0 ] && echo "true" || echo "false")
 echo output_dir = $OUTPUT_DIR
 echo data_train = $DATA_TRAIN_DIR, data_ft = $DATA_FT_DIR
@@ -21,4 +22,4 @@ python run_model.py --do_train --do_validation --data_dir $DATA_TRAIN_DIR --outp
 	--num_train_epochs $train_epochs
 python run_model.py --train_scd --do_train --do_validation --data_dir $DATA_FT_DIR --output_dir ${OUTPUT_DIR}/finetune/ --gradient_accumulation_steps $grad_acc_steps \
 	--pool_type $pool --target_embeddings $targ_emb --head_batchnorm $batch_norm --loss $ft_loss --linear_head ${linhead} --head_hidden_size $hs \
-	--num_train_epochs $ft_epochs --ckpt_path ${OUTPUT_DIR}/train/nen-nen-weights/
+	--num_train_epochs $ft_epochs --ckpt_path ${OUTPUT_DIR}/train/${train_ckpt}/
