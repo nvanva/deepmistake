2609333d143328ec8aa3ef26e10695bd90491a3b
ameta13 <maxim.fedoseev13@yandex.ru>
2021-03-17 02:12:34 +0300

    predict_scores.sh:: Add explanations about argument

--- predict_scores.sh
+++ predict_scores.sh
@@ -1,4 +1,4 @@
-ckpt_path=$1  # path to directory with checkpoint, args.json and pytorch_model.bin must be in {ckpt_path} directory!
+ckpt_path=$1  # ABSOLUTE path to directory with checkpoint, args.json and pytorch_model.bin must be in {ckpt_path} directory!
 echo ckpt_path=$ckpt_path
 for part in test_RuShiftEval rusemshift-data/dev rusemshift-data/train; do
 python run_model.py --do_eval --ckpt_path $ckpt_path --eval_input_dir data_dumped_full/$part/ \
