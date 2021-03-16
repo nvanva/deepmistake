93c117fa9e1b6c476ca9a98c6eec27f80e09e46a
Maksim Fedoseev <maxim.fedoseev13@gmail.com>
2021-03-17 00:19:10 +0300

    Wrote original --save_by_score for train_concat_first_symmetric_scd_train.sh

--- train_concat_first_symmetric_scd_train.sh
+++ train_concat_first_symmetric_scd_train.sh
@@ -11,5 +11,5 @@ targ_emb=concat
 hs=-1
 train_ckpt=accuracy.dev.en-en.score
 OUTPUT_DIR=xlmrlarge-train_loss-${train_loss}-ft_loss-${ft_loss}-pool-${pool}-targ_emb-${targ_emb}-hs-${hs}-bn-${batch_norm}-ckpt-${train_ckpt}
-ft_save_by_score=spearman.dev.scd_1.score+spearman.dev.scd_2.score+spearman.dev.scd_1.wordwise.score+spearman.dev.scd_2.wordwise.score
+ft_save_by_score=spearman.dev.scd_1.score+spearman.dev.scd_2.score
 bash train_n_finetune_model.sh $train_loss $ft_loss $pool $targ_emb $batch_norm $hs $train_epochs $ft_epochs $grad_acc_steps $train_ckpt $OUTPUT_DIR $DATA_TRAIN_DIR $ft_save_by_score $DATA_FT_DIR
