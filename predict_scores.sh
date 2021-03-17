d28dd448c6b0a50194f86cea247475809300bd1c
ameta13 <maxim.fedoseev13@yandex.ru>
2021-03-17 13:21:07 +0300

    Add to predict_scores.sh eval on sampled datasets

--- predict_scores.sh
+++ predict_scores.sh
@@ -1,6 +1,6 @@
 ckpt_path=$1  # ABSOLUTE path to directory with checkpoint, args.json and pytorch_model.bin must be in {ckpt_path} directory!
 echo ckpt_path=$ckpt_path
-for part in test_RuShiftEval rusemshift-data/dev rusemshift-data/train; do
+for part in test_RuShiftEval rusemshift-data/dev rusemshift-data/train sampled_test_RuShiftEval sampled_rusemshift/dev sampled_rusemshift/train; do
 python run_model.py --do_eval --ckpt_path $ckpt_path --eval_input_dir data_dumped_full/$part/ \
   --eval_output_dir $ckpt_path/scores/$part/ --output_dir $ckpt_path
 done
