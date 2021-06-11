0b19d380ee6c709e4bcbec1d57ee403c1b9e8f63
Maksim Fedoseev <maxim.fedoseev13@gmail.com>
2021-06-11 13:48:53 +0300

    Fix bug in datasets_bruteforce.sh with OUTPUT_DIR

--- datasets_bruteforce.sh
+++ datasets_bruteforce.sh
@@ -28,7 +28,7 @@ else
 	ft_save_by_score=accuracy.dev.nen-nen.score
 fi
 DATA_FT_DIR=data_dumped_full/${dataset}/
-OUTPUT_DIR=xlmr-large..data_train-${dataset}..train_loss-${train_loss}..data_ft-${dataset}..ft_loss-${ft_loss}..pool-${pool}..targ_emb-${targ_emb}..hs-${hs}..bn-${batch_norm}..ckpt-${train_ckpt}
+OUTPUT_DIR=xlmr-large..data_train-wic..train_loss-${train_loss}..data_ft-${dataset}..ft_loss-${ft_loss}..pool-${pool}..targ_emb-${targ_emb}..hs-${hs}..bn-${batch_norm}..ckpt-${train_ckpt}
 bash train_n_finetune_model.sh $train_loss $ft_loss $pool $targ_emb $batch_norm $hs $train_epochs $ft_epochs $grad_acc_steps $train_ckpt $OUTPUT_DIR $DATA_TRAIN_DIR $ft_save_by_score $DATA_FT_DIR
 done
 
@@ -37,14 +37,14 @@ ft_save_by_score=spearman.dev.scd_1.score+spearman.dev.scd_2.score+spearman.dev.
 ft_loss=mse_loss
 dataset=rusemshift-data
 DATA_FT_DIR=data_dumped_full/${dataset}/
-OUTPUT_DIR=xlmr-large..data_train-${dataset}..train_loss-${train_loss}..data_ft-${dataset}..ft_loss-${ft_loss}..pool-${pool}..targ_emb-${targ_emb}..hs-${hs}..bn-${batch_norm}..ckpt-${train_ckpt}
+OUTPUT_DIR=xlmr-large..data_train-wic..train_loss-${train_loss}..data_ft-${dataset}..ft_loss-${ft_loss}..pool-${pool}..targ_emb-${targ_emb}..hs-${hs}..bn-${batch_norm}..ckpt-${train_ckpt}
 bash train_n_finetune_model.sh $train_loss $ft_loss $pool $targ_emb $batch_norm $hs $train_epochs $ft_epochs $grad_acc_steps $train_ckpt $OUTPUT_DIR $DATA_TRAIN_DIR $ft_save_by_score $DATA_FT_DIR
 
 # mcl-wic full -> RuSemShift + wic ru-ru (mse+ loss)
 ft_loss=mseplus_loss
 dataset=rusemshift-ruwic-data
 DATA_FT_DIR=data_dumped_full/${dataset}/
-OUTPUT_DIR=xlmr-large..data_train-${dataset}..train_loss-${train_loss}..data_ft-${dataset}..ft_loss-${ft_loss}..pool-${pool}..targ_emb-${targ_emb}..hs-${hs}..bn-${batch_norm}..ckpt-${train_ckpt}
+OUTPUT_DIR=xlmr-large..data_train-wic..train_loss-${train_loss}..data_ft-${dataset}..ft_loss-${ft_loss}..pool-${pool}..targ_emb-${targ_emb}..hs-${hs}..bn-${batch_norm}..ckpt-${train_ckpt}
 bash train_n_finetune_model.sh $train_loss $ft_loss $pool $targ_emb $batch_norm $hs $train_epochs $ft_epochs $grad_acc_steps $train_ckpt $OUTPUT_DIR $DATA_TRAIN_DIR $ft_save_by_score $DATA_FT_DIR
 
 # RuSemShift (MSE loss)
