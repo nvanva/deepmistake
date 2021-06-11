396956451017b4a71de0b2eb7643e568402a6c11
Maksim Fedoseev <maxim.fedoseev13@gmail.com>
2021-06-11 14:57:28 +0300

    Remove comment

--- all_ckpt_predict_scores.sh
+++ all_ckpt_predict_scores.sh
@@ -1,6 +1,5 @@
 root=$(readlink --canonicalize $(dirname $0)) # absolute path of this script
 echo root = $root
 for ckpt in $root/xlmr-large..*dist_l1ndotn..hs-0..bn-1*/*/nen-nen-weights; do
-	echo bash predict_scores.sh $root/${ckpt}
-	#bash predict_scores.sh $root/${ckpt}
+	bash predict_scores.sh $root/${ckpt}
 done
