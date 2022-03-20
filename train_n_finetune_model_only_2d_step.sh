d90293ea421e3cba6b78219eab77fc17745e9151
Daniil <homdanil123@gmail.com>
2022-03-20 22:38:08 +0300

    fix

--- train_n_finetune_model_only_2d_step.sh
+++ train_n_finetune_model_only_2d_step.sh
@@ -13,8 +13,8 @@ grad_acc_steps=$9 #4
 train_ckpt=${10} #nen-nen-weights or accuracy.dev.en-en.score
 OUTPUT_DIR=${11} #xlmrlarge-train_loss-${train_loss}-ft_loss-${ft_loss}-pool-${pool}-targ_emb-${targ_emb}-hs-${hs}-bn-${batch_norm}-ckpt-${train_ckpt}
 DATA_TRAIN_DIR=${12} #data/wic/
-ft_save_by_score=${13} #spearman.dev.scd_1.score+spearman.dev.scd_2.score+spearman.dev.scd_1.wordwise.score+spearman.dev.scd_2.wordwise.score
-DATA_FT_DIR=${14} #data/rusemshift-data/
+#ft_save_by_score=${13} #spearman.dev.scd_1.score+spearman.dev.scd_2.score+spearman.dev.scd_1.wordwise.score+spearman.dev.scd_2.wordwise.score
+DATA_FT_DIR=${13} #data/rusemshift-data/
 
 linhead=$([ "$hs" == 0 ] && echo "true" || echo "false")
 train_scd=--train_scd
@@ -29,7 +29,7 @@ echo train_epochs = $train_epochs, ft_epochs = $ft_epochs, grad_acc_steps = $gra
 if [ -n "$DATA_FT_DIR" ]; then
 	python run_model.py $train_scd --do_train --do_validation --data_dir $DATA_FT_DIR --output_dir ${OUTPUT_DIR}/finetune/ --gradient_accumulation_steps $grad_acc_steps \
 		--pool_type $pool --target_embeddings $targ_emb --head_batchnorm $batch_norm --loss $ft_loss --linear_head ${linhead} --head_hidden_size $hs \
-		--num_train_epochs $ft_epochs --ckpt_path ${train_ckpt}/ --save_by_score $ft_save_by_score
+		--num_train_epochs $ft_epochs --ckpt_path ${train_ckpt}/ 
 else
 	echo Without fine-tuning
 fi
