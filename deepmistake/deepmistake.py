b7e91ab2797d4e2a537672d3a1716bbae425a79c
arshandalili <arshandalili@gmail.com>
2024-09-10 15:10:29 +0200

    Some modifications

--- deepmistake/deepmistake.py
+++ deepmistake/deepmistake.py
@@ -41,9 +41,7 @@ class DeepMistakeWiC:
             data_processor=data_processor,
             config=self.config,
             local_config=self.local_config,
-        ).to(
-            device
-        )
+        ).to(self.device)
 
     def predict_examples(self, examples: list[Example], log, batch_size=16):
         """
@@ -52,15 +50,10 @@ class DeepMistakeWiC:
         # Runs inference, similarly to run_model.py, lines 624-634
         test_features = self.model.convert_examples_to_features(examples, log)
         eval_dataloader = get_dataloader_and_tensors(test_features, batch_size)
-        metrics, syns_preds, syns_scores_res = self.predict(
+        syns_preds, syns_scores_res = self.predict(
             model=self.model,
             eval_dataloader=eval_dataloader,
-            eval_fearures=test_features,
-            compute_metrics=False,
-            dump_feature=False,
-            output_dir='dm_testwug',
         )
-        print(syns_scores_res.shape, syns_preds.shape)
         return syns_scores_res, syns_preds
 
     def predict_dataset(self, test_dir, output_dir, eval_output_dir):
@@ -72,36 +65,19 @@ class DeepMistakeWiC:
             test_features, self.local_config["eval_batch_size"]
         )
 
-        metrics, syns_preds, syns_scores_res = self.predict(
+        syns_preds, syns_scores_res = self.predict(
             self.model,
             eval_dataloader,
-            os.path.join(output_dir, eval_output_dir),
-            test_features,
-            compute_metrics=True,
         )
 
-        print(metrics)
-        with open(
-            os.path.join(output_dir, eval_output_dir, "metrics.txt"), "w"
-        ) as outp:
-            print(metrics, file=outp)
+        return syns_scores_res, syns_preds
 
     def predict(
         self,
         model,
         eval_dataloader,
-        output_dir,
-        eval_fearures,
-        cur_train_mean_loss=None,
-        compute_metrics=True,
         only_parts="",
-        dump_feature=True,
     ):
-
-        if os.path.exists(output_dir):
-            os.system(f"rm -r {output_dir}/*")
-        else:
-            os.makedirs(output_dir, exist_ok=True)
 
         only_parts = [part for part in only_parts.split("+") if part]
         model.eval()
@@ -110,135 +86,15 @@ class DeepMistakeWiC:
         eval_dataloader = tqdm(
             eval_dataloader, total=len(eval_dataloader), desc="validation ... "
         )
-        metrics, syns_preds, syns_scores_res = self.run_inference(
+        syns_preds, syns_scores_res = self.run_inference(
             eval_dataloader,
             model,
             self.device,
-            compute_metrics,
-            dump_feature,
-            output_dir,
         )
 
-        predictions = defaultdict(lambda: defaultdict(list))
-        golds = defaultdict(lambda: defaultdict(list))
-        scores = defaultdict(lambda: defaultdict(list))
-        gold_scores = defaultdict(lambda: defaultdict(list))
-        lemmas = defaultdict(lambda: defaultdict(list))
+        return syns_preds, syns_scores_res
 
-        syn_ids_to_label = {0: "F", 1: "T"}
-        for ex_id, (ex_feature, ex_syn_preds, ex_scores) in enumerate(
-            zip(eval_fearures, syns_preds, syns_scores_res)
-        ):
-            example = ex_feature.example
-            docId = example.docId
-            posInDoc = int(docId.split(".")[-1])
-            docId = ".".join(docId.split(".")[:-1])
-            syn_pred = syn_ids_to_label[ex_syn_preds.item()]
-            predictions[docId][posInDoc].append(syn_pred)
-            golds[docId][posInDoc].append(example.label)
-            scores[docId][posInDoc].append(ex_scores)
-            gold_scores[docId][posInDoc].append(example.score)
-            lemmas[docId][posInDoc].append((example.lemma, example.grp))
-
-        print(f"saving predictions for: {only_parts}")
-        for docId, doc_preds in predictions.items():
-            doc_scores = scores[docId]
-            if len(only_parts) > 0 and all(
-                [f'{docId.split(".")[1]}.score' not in part for part in only_parts]
-            ):
-                continue
-            print(f"saving predictions for part: {docId}")
-            prediction = [
-                {"id": f"{docId}.{pos}", "tag": "F" if "F" in doc_preds[pos] else "T"}
-                for pos in sorted(doc_preds)
-            ]
-            prediction_file = os.path.join(output_dir, docId)
-            json.dump(prediction, open(prediction_file, "w"))
-            prediction = [
-                {"id": f"{docId}.{pos}", "score": [str(x) for x in doc_scores[pos]]}
-                for pos in sorted(doc_preds)
-            ]
-            prediction_file = os.path.join(output_dir, f"{docId}.scores")
-            json.dump(prediction, open(prediction_file, "w"))
-
-        if compute_metrics:
-            mean_non_english = []
-            for docId, doc_preds in predictions.items():
-                if "scd" in docId:
-                    doc_golds = gold_scores[docId]
-                    doc_lemmas = lemmas[docId]
-                    doc_scores = scores[docId]
-
-                    keys = sorted(list(doc_golds.keys()))
-                    # print(doc_lemmas)
-                    unique_lemmas = sorted(
-                        set(
-                            [
-                                doc_lemmas[key][0][0]
-                                for key in keys
-                                if doc_lemmas[key][0][1] == "COMPARE"
-                            ]
-                        )
-                    )
-                    y_true, y_pred = [], []
-                    y_sent_true, y_sent_pred = [], []
-                    for unique_lemma in unique_lemmas:
-                        unique_lemma_keys = [
-                            key
-                            for key in keys
-                            if doc_lemmas[key][0][0] == unique_lemma
-                            and doc_lemmas[key][0][1] == "COMPARE"
-                        ]
-                        unique_word_scores_pred = [
-                            np.array(doc_scores[key]).mean()
-                            for key in unique_lemma_keys
-                        ]
-                        unique_word_scores_true = [
-                            doc_golds[key][0] for key in unique_lemma_keys
-                        ]
-                        y_true.append(np.array(unique_word_scores_true).mean())
-                        y_pred.append(np.array(unique_word_scores_pred).mean())
-                        y_sent_true.extend(unique_word_scores_true)
-                        y_sent_pred.extend(unique_word_scores_pred)
-                    metrics[f"spearman.{docId}.wordwise.score"], _ = spearmanr(
-                        y_true, y_pred
-                    )
-                    metrics[f"spearman.{docId}.score"], _ = spearmanr(
-                        y_sent_true, y_sent_pred
-                    )
-                    doc_golds = golds[docId]
-                    keys = list(doc_golds.keys())
-                    doc_golds = [doc_golds[key][0] for key in keys]
-                    doc_preds = ["F" if "F" in doc_preds[key] else "T" for key in keys]
-                    metrics[f"{docId}.accuracy"] = accuracy_score(doc_golds, doc_preds)
-                else:
-                    doc_golds = golds[docId]
-                    keys = list(doc_golds.keys())
-                    doc_golds = [doc_golds[key][0] for key in keys]
-                    doc_preds = ["F" if "F" in doc_preds[key] else "T" for key in keys]
-                    metrics[f"accuracy.{docId}.score"] = accuracy_score(
-                        doc_golds, doc_preds
-                    )
-                    if "en-en" not in docId:
-                        mean_non_english.append(metrics[f"accuracy.{docId}.score"])
-            if mean_non_english:
-                metrics[f'accuracy.{docId.split(".")[0]}.nen-nen.score'] = sum(
-                    mean_non_english
-                ) / len(mean_non_english)
-
-            if cur_train_mean_loss is not None:
-                metrics.update(cur_train_mean_loss)
-        else:
-            metrics = {}
-
-        model.train()
-
-        return metrics, syns_preds, syns_scores_res
-
-    def run_inference(
-        self, eval_dataloader, model, device, compute_metrics, dump_feature, output_dir
-    ):
-        metrics = defaultdict(float)
+    def run_inference(self, eval_dataloader, model, device):
         nb_eval_steps = 0
         syns_preds = []
         for batch_id, batch in enumerate(eval_dataloader):
@@ -253,24 +109,6 @@ class DeepMistakeWiC:
                     input_labels={"syn_labels": b_syn_labels, "positions": b_positions},
                     return_features=True,
                 )
-
-            if dump_feature:
-                syn_features = syn_features.detach().cpu().numpy()
-                np.save(
-                    os.path.join(output_dir, f"features_batch_{batch_id}.npy"),
-                    syn_features,
-                )
-                np.save(
-                    os.path.join(output_dir, f"labels_batch_{batch_id}.npy"),
-                    b_syn_labels.detach().cpu().numpy(),
-                )
-
-            if compute_metrics:
-                for key, value in loss.items():
-                    metrics[f"eval_{key}_loss"] = (
-                        metrics[f"eval_{key}_loss"] * nb_eval_steps
-                        + value.mean().item()
-                    ) / (nb_eval_steps + 1)
 
             nb_eval_steps += 1
             if model.local_config["loss"] != "cosine_similarity":
@@ -304,4 +142,4 @@ class DeepMistakeWiC:
                 syns_preds[syns_scores >= 3.0] = 1
             else:
                 syns_preds[syns_scores > 0.5] = 1
-        return metrics, syns_preds, syns_scores_res
+        return syns_preds, syns_scores_res
